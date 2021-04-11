f375e34c029157a747be2972d213e38688d902ef
HILMI ONELL <57817028+HILMIONELL97@users.noreply.github.com>
2021-04-11 22:13:11 +0000

    Init-React-App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import Contact from './components/Contacts/Contact';
+
+ class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>HELLO FROM REACT APP</h1>
+      <Contact />
+      </div>
+    )
+  }
+}
+
+export default App
